15d1a8d67dc90106099544d95a080bc75e6eda6d
Mar <marina.falcon@gmail.com>
2023-01-08 02:03:45 +0100

    Check routes and validate requests

--- app.js
+++ app.js
@@ -21,7 +21,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-// app.use('/pdf', express.static('D:\pdfs'))
 
 /**
  * Rutas de mi API
